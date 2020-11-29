1407598d9cea59acd18e405ff9995137e4192f38
Aleks Kocharian <rozrobka@yahoo.com>
2020-11-29 21:23:33 +0200

    add basic page structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './components/main/main.component';
+import { SearchComponent } from './views/search/search.component';
+import { AboutComponent } from './views/about/about.component';
+import { Page404Component } from './views/page404/page404.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'search',
+        component: SearchComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'search',
+        pathMatch: 'full',
+      },
+      { path: '**', component: Page404Component },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SearchComponent } from './views/search/search.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { AboutComponent } from './views/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Page404Component } from './views/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    SidebarComponent,
+    SearchComponent,
+    SearchFormComponent,
+    AboutComponent,
+    FooterComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
